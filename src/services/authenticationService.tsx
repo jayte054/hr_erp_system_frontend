227c49b3958be933149987c234788b61690f176b
jayte054 <54476858+jayte054@users.noreply.github.com>
2024-12-04 11:08:22 +0100

    update

--- src/services/authenticationService.tsx
+++ src/services/authenticationService.tsx
@@ -2,13 +2,23 @@ import axios from 'axios';
 import { AdminSignup, EmployeeSignup, SignInData } from "../types/types";
 import { toastify } from '../components/utils';
 
-export const Base_Url = "http://localhost:3001"
-// export const Base_Url = "https://hr-erp-system-backend.onrender.com"
+// export const Base_Url = "http://localhost:3001"
+export const Base_Url = "https://hr-erp-system-backend.onrender.com"
 export const AuthenticationService = {
     adminSignup: async (formData: AdminSignup) => {
         try {
-            const adminSignup = await axios.post(`${Base_Url}/api/user/adminSignup`, formData)
-            console.log(adminSignup.data)
+            // const adminSignup = await axios.post(`${Base_Url}/api/user/adminSignup`, formData)
+            // console.log(adminSignup.data)
+            const response = await fetch(`${Base_Url}/api/user/signin`, {
+                method: 'POST', 
+                mode: 'no-cors',
+                headers: {
+                  'Content-Type': 'application/json', // Specify JSON format
+                },
+                body: JSON.stringify(formData), // Convert data to JSON string
+              });
+              const data = await response.json();
+              return data;
         } catch (error) {
             console.log(error)
             toastify.signupSuccessful('admin signup successful')
