edd302fe2ac76dddfc47c95b94b878cc0d1211ba
jayte054 <54476858+jayte054@users.noreply.github.com>
2024-12-04 15:01:20 +0100

    update

--- src/services/authenticationService.tsx
+++ src/services/authenticationService.tsx
@@ -3,7 +3,7 @@ import { EmployeeSignup, SignInData } from "../types/types";
 import { toastify } from '../components/utils';
 
 // export const Base_Url = "http://localhost:3001"
-export const Base_Url = "https://hr-erp-system-backend.onrender.com/"
+export const Base_Url = "https://hr-erp-system-backend-1.onrender.com"
 export const AuthenticationService = {
     adminSignup: async (formData:{
         name: string;
