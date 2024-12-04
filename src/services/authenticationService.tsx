dc91ef812551a325d80ff765db6d8535ad1d5fc8
jayte054 <54476858+jayte054@users.noreply.github.com>
2024-12-04 08:25:14 +0100

    adding deployed url

--- src/services/authenticationService.tsx
+++ src/services/authenticationService.tsx
@@ -6,7 +6,6 @@ import { toastify } from '../components/utils';
 export const Base_Url = "https://hr-erp-system-backend.onrender.com"
 export const AuthenticationService = {
     adminSignup: async (formData: AdminSignup) => {
-        console.log(formData)
         try {
             const adminSignup = await axios.post(`${Base_Url}/api/user/adminSignup`, formData)
             console.log(adminSignup.data)
